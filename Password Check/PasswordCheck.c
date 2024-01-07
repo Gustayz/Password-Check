d62ba0d441aa9b0c44eabee0ff9a4fc1eb9b2c92
Gustayz <114109045+Gustayz@users.noreply.github.com>
2024-01-06 23:36:16 -0500

    Update: Prompted the user to input a password, then the console will stored it in the variable userPassword.

--- Password Check/PasswordCheck.c
+++ Password Check/PasswordCheck.c
@@ -2,6 +2,10 @@
 
 int main()
 {
+
+	int userPassword;
 	printf("PasswordCheck by Gustavo Yepez.");
 	printf("Hello there and welcome to my program that checks if a password is good enough to pass.");
+	printf("Please insert your password down below:");
+	scanf_s("%i", userPassword);
 }
