48a00b239a20fd2a9630cff7d07048cfe7098e91
Gustayz <114109045+Gustayz@users.noreply.github.com>
2024-01-07 21:03:04 -0500

    Update: Declared a method that will decide if the password is good enough to pass. (still in progress)

--- Password Check/PasswordCheck.c
+++ Password Check/PasswordCheck.c
@@ -1,11 +1,21 @@
 #include <stdio.h>
 
+int PasswordCheck();
+
 int main()
 {
 
 	int userPassword;
-	printf("PasswordCheck by Gustavo Yepez.");
-	printf("Hello there and welcome to my program that checks if a password is good enough to pass.");
-	printf("Please insert your password down below:");
-	scanf_s("%i", userPassword);
+	printf("\n PasswordCheck by Gustavo Yepez.");
+	printf("\n Hello there and welcome to my program that checks if a password is good enough to pass.");
+	printf("\n Please insert your password down below:\n ");
+	scanf_s("%i", &userPassword);
+
+}
+
+int PasswordCheck(int userAnswer)
+{
+	
+	
+	return userAnswer;
 }
