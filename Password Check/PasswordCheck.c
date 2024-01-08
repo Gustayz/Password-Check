eb9200ea226ed8ebc168ee4e899274f5d7e41dd7
Gustayz <114109045+Gustayz@users.noreply.github.com>
2024-01-08 12:10:28 -0500

    Update: Added comments on the project for more comprehension.

--- Password Check/PasswordCheck.c
+++ Password Check/PasswordCheck.c
@@ -4,13 +4,22 @@ int PasswordCheck();
 
 int main()
 {
+	// variable userPassword created to store the user password.
 
 	int userPassword;
+
+	// prompting the user with some info about the program and asking about to insert a password of their choice.
+
 	printf("\n PasswordCheck by Gustavo Yepez.");
 	printf("\n Hello there and welcome to my program that checks if a password is good enough to pass.");
 	printf("\n Please insert your password down below:\n ");
+
+	// assigning the user password into the variable userPassword.
+
 	scanf_s("%i", &userPassword);
 
+	// 
+	int finalAnswer = PasswordCheck(userPassword);
 }
 
 int PasswordCheck(int userAnswer)
